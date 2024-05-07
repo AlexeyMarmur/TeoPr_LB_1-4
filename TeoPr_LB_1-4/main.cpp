e2d61632ed96f10df0dbaf4bc5036c9b44542324
AlexeyMarmur <alexeymarmur@gmail.com>
2024-05-07 11:48:17 +0300

    Refactoring code

--- TeoPr_LB_1-4/main.cpp
+++ TeoPr_LB_1-4/main.cpp
@@ -22,11 +22,105 @@ int initial[4][4] = {
     { 1, 2, 3, 4}
 };
 int criteriaValues[4][4] = {
-    { 1111, 1111, 1111, 1111},
+  { 1111, 1111, 1111, 1111},
   { 2111, 1211, 1121, 1112},
   { 3111, 1311, 1131, 1113},
   { 4111, 1411, 1141, 1114}
 };
+
+void fillDiagonalWithTwo(vector<vector<int>>& matrix, const vector<string>& numbers);
+void compareAndFillMatrix(vector<vector<int>>& matrix, const vector<string>& numbers);
+void printInitialAndFinalMatrix(const vector<vector<int>>& matrix, const vector<string>& numbers);
+void printAlternatives(const vector<string>& numbers, const vector<pair<string, int>>& ranked_numbers);
+void createRankedNumbers(const vector<string>& numbers, const vector<string>& epors);
+void printVectorValuation(const int initial[4][4]);
+void createInitialByEporsMatrix(const int initial[4][4], const map<string, int>& eporsToIndex, int initialByEpors[4][4]);
+void printInitialByEporsMatrix(const int initialByEpors[4][4]);
+void createSortedInitialByEporsMatrix(const int initialByEpors[4][4], int sortedInitialByEpors[4][4]);
+void printSortedInitialByEporsMatrix(const int sortedInitialByEpors[4][4]);
+void findBestAlternative(const int sortedInitialByEpors[4][4]);
+void test_compareNumbers(double& tests_passed);
+void test_matrix_initialization(double& tests_passed);
+void test_ranked_numbers(double& tests_passed);
+void test_updateTransitiveRelations(double& tests_passed);
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    char user_choice;
+    cout << "Enter 'R' to run the program or 'T' to run tests: ";
+    cin >> user_choice;
+
+    if (user_choice == 'T') {
+        double tests_passed = 0;
+        double all_tests = 4;
+        cout << "Running tests..." << endl << endl;
+        test_compareNumbers(tests_passed);
+        test_matrix_initialization(tests_passed);
+        test_ranked_numbers(tests_passed);
+        test_updateTransitiveRelations(tests_passed);
+
+        cout << "Values of passed tests: " << tests_passed << endl;
+
+        if (tests_passed == all_tests)
+        {
+            cout << "All tests passed - 100%" << endl;
+        }
+        else
+        {
+            cout << "Not all tests have been passed. Passed : " << tests_passed << " of " << all_tests << endl;
+            cout << "Percentage: " << (tests_passed / all_tests) * 100 << "%" << endl << endl;
+        }
+
+    }
+    else if (user_choice == 'R') {
+
+        cout << "Running program..." << endl << endl;
+
+        // Matrix to store the comparison results
+        vector<vector<int>> matrix = {
+          {2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+          {0, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+          {0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+          {0, 0, 0, 2, 1, 1, 0, 0, 0, 0, 0, 0},
+          {0, 0, 0, 0, 2, 1, 0, 0, 0, 0, 0, 0},
+          {0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0},
+          {0, 0, 0, 0, 0, 0, 2, 1, 1, 0, 0, 0},
+          {0, 0, 0, 0, 0, 0, 0, 2, 1, 0, 0, 0},
+          {0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0},
+          {0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 1},
+          {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1},
+          {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2}
+        };
+
+        fillDiagonalWithTwo(matrix, numbers);
+        compareAndFillMatrix(matrix, numbers);
+        printInitialAndFinalMatrix(matrix, numbers);
+        printAlternatives(numbers, ranked_numbers);
+        printVectorValuation(initial);
+
+        map<string, int> eporsToIndex;
+        for (int i = 0; i < epors.size(); ++i) {
+            eporsToIndex[epors[i]] = i + 1;
+        }
+
+        int initialByEpors[4][4];
+        createInitialByEporsMatrix(initial, eporsToIndex, initialByEpors);
+        printInitialByEporsMatrix(initialByEpors);
+
+        int sortedInitialByEpors[4][4];
+        createSortedInitialByEporsMatrix(initialByEpors, sortedInitialByEpors);
+        printSortedInitialByEporsMatrix(sortedInitialByEpors);
+
+        findBestAlternative(sortedInitialByEpors);
+    }
+
+    else {
+        cout << "Invalid choice. Please enter 'R' or 'T'." << std::endl;
+    }
+
+    return 0;
+}
 
 // Function to compare two numbers based on user input and existing matrix
 int compareNumbers(const string& num1, const string& num2, vector<vector<int>>& matrix)
@@ -118,6 +212,145 @@ void printMatrix(const vector<string>& numbers, const vector<vector<int>>& matri
         }
         cout << endl;
     }
+}
+
+void fillDiagonalWithTwo(vector<vector<int>>& matrix, const vector<string>& numbers) {
+    for (int i = 0; i < numbers.size(); ++i) {
+        matrix[i][i] = 2;
+    }
+}
+
+void compareAndFillMatrix(vector<vector<int>>& matrix, const vector<string>& numbers) {
+    cout << "Initial matrix:" << endl;
+    for (int i = 0; i < numbers.size(); ++i) {
+        for (int j = i + 1; j < numbers.size(); ++j) {
+            cout << "print 1 if better, 2 if equal, 3 if worse" << endl;
+            cout << "Comparing " << numbers[i] << " and " << numbers[j] << ": " << endl;
+            printMatrix(numbers, matrix);
+            int result = compareNumbers(numbers[i], numbers[j], matrix);
+            updateTransitiveRelations(matrix);
+            cout << endl;
+        }
+    }
+    cout << "Final matrix:" << endl;
+    printMatrix(numbers, matrix);
+}
+
+void printInitialAndFinalMatrix(const vector<vector<int>>& matrix, const vector<string>& numbers) {
+    cout << "Initial matrix:" << endl;
+    printMatrix(numbers, matrix);
+    cout << "Final matrix:" << endl;
+    printMatrix(numbers, matrix);
+}
+
+void printAlternatives(const vector<string>& numbers, const vector<pair<string, int>>& ranked_numbers) {
+    cout << "A set of alternatives to the first reference situation:" << endl;
+    for (const auto& num : numbers) {
+        cout << num << "    ";
+    }
+    cout << endl;
+
+    cout << "Sorted set of alternatives to the first reference situation:" << endl;
+    for (const auto& num : ranked_numbers) {
+        cout << num.first << "\t";
+    }
+    cout << endl;
+
+    cout << "Single ordinal scale" << endl;
+    for (const auto& ep : epors) {
+        cout << ep << "\t";
+    }
+    cout << endl;
+}
+
+void createRankedNumbers(const vector<string>& numbers, const vector<string>& epors) {
+    ranked_numbers.clear();
+    for (int i = 0; i < numbers.size(); ++i) {
+        ranked_numbers.push_back(make_pair(numbers[i], i + 1));
+    }
+    sort(ranked_numbers.begin(), ranked_numbers.end(), [&](const pair<string, int>& a, const pair<string, int>& b) {
+        int posA = find(epors.begin(), epors.end(), a.first) - epors.begin();
+        int posB = find(epors.begin(), epors.end(), b.first) - epors.begin();
+        return posA < posB;
+        });
+}
+
+void printVectorValuation(const int initial[4][4]) {
+    cout << "Vector valuation (initial):" << endl;
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            cout << initial[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void createInitialByEporsMatrix(const int initial[4][4], const map<string, int>& eporsToIndex, int initialByEpors[4][4]) {
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            int criterionIndex = initial[i][j] - 1;
+            string criterion = to_string(criteriaValues[criterionIndex][j]);
+
+            // Check if criterion exists in eporsToIndex
+            if (eporsToIndex.count(criterion) > 0) {
+                int rank = eporsToIndex.at(criterion); // Use .at() to access value safely
+                initialByEpors[i][j] = rank;
+            }
+            else {
+                // Handle case where criterion is not found in eporsToIndex
+                // You can set a default value or handle this case accordingly
+                cerr << "Error: Criterion '" << criterion << "' not found in eporsToIndex." << endl;
+                // For example, you could assign a default value:
+                initialByEpors[i][j] = -1; // Or another suitable value
+            }
+        }
+    }
+}
+
+
+void printInitialByEporsMatrix(const int initialByEpors[4][4]) {
+    cout << "Initial by a single ordinal scale:" << endl;
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            cout << initialByEpors[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void createSortedInitialByEporsMatrix(const int initialByEpors[4][4], int sortedInitialByEpors[4][4]) {
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            sortedInitialByEpors[i][j] = initialByEpors[i][j];
+        }
+        sort(sortedInitialByEpors[i], sortedInitialByEpors[i] + 4);
+    }
+}
+
+void printSortedInitialByEporsMatrix(const int sortedInitialByEpors[4][4]) {
+    cout << "Sorted Initial by a single ordinal scale:" << endl;
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            cout << sortedInitialByEpors[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void findBestAlternative(const int sortedInitialByEpors[4][4]) {
+    int minRowIndex = 0;
+    for (int i = 1; i < 4; ++i) {
+        if (sortedInitialByEpors[i][0] < sortedInitialByEpors[minRowIndex][0]) {
+            minRowIndex = i;
+        }
+    }
+    cout << "The best alternative:" << endl;
+    for (int j = 0; j < 4; ++j) {
+        cout << sortedInitialByEpors[minRowIndex][j] << " ";
+    }
+    cout << endl;
 }
 
 //
@@ -272,205 +505,3 @@ void test_updateTransitiveRelations(double& tests_passed)
         cout << "test_updateTransitiveRelations failed." << endl << endl;
     }
 }
-int main()
-{
-    setlocale(LC_ALL, "Russian");
-    char user_choice;
-    cout << "Enter 'R' to run the program or 'T' to run tests: ";
-    cin >> user_choice;
-
-    if (user_choice == 'T') {
-        double tests_passed = 0;
-        double all_tests = 4;
-        cout << "Running tests..." << endl << endl;
-        test_compareNumbers(tests_passed);
-        test_matrix_initialization(tests_passed);
-        test_ranked_numbers(tests_passed);
-        test_updateTransitiveRelations(tests_passed);
-
-        cout << "Values of passed tests: " << tests_passed << endl;
-
-        if (tests_passed == all_tests)
-        {
-            cout << "All tests passed - 100%" << endl;
-        }
-        else
-        {
-            cout << "Not all tests have been passed. Passed : " << tests_passed << " of " << all_tests << endl;
-            cout << "Percentage: " << (tests_passed / all_tests) * 100 << "%" << endl << endl;
-        }
-
-    }
-    else if (user_choice == 'R') {
-
-        cout << "Running program..." << endl << endl;
-
-        // Matrix to store the comparison results
-        vector<vector<int>> matrix = {
-          {2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-          {0, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-          {0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-          {0, 0, 0, 2, 1, 1, 0, 0, 0, 0, 0, 0},
-          {0, 0, 0, 0, 2, 1, 0, 0, 0, 0, 0, 0},
-          {0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0},
-          {0, 0, 0, 0, 0, 0, 2, 1, 1, 0, 0, 0},
-          {0, 0, 0, 0, 0, 0, 0, 2, 1, 0, 0, 0},
-          {0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0},
-          {0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 1},
-          {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1},
-          {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2}
-        };
-
-        // Fill in the diagonal elements with 2
-        for (int i = 0; i < numbers.size(); ++i)
-        {
-            matrix[i][i] = 2;
-        }
-        cout << "Initial matrix:" << endl;
-        // Compare each pair of numbers and fill in the matrix
-        for (int i = 0; i < numbers.size(); ++i)
-        {
-            for (int j = i + 1; j < numbers.size(); ++j)
-            {
-                cout << "print 1 if better, 2 if equal, 3 if worse" << endl;
-                cout << "Comparing " << numbers[i] << " and " << numbers[j] << ": " << endl;
-                printMatrix(numbers, matrix);
-                int result = compareNumbers(numbers[i], numbers[j], matrix);
-                updateTransitiveRelations(matrix); // Apply transitive property after every comparison
-                cout << endl;
-            }
-        }
-
-        // Print the final matrix
-        cout << "Final matrix:" << endl;
-        printMatrix(numbers, matrix);
-
-        cout << "A set of alternatives to the first reference situation:" << endl;
-        for (const auto& num : numbers) {
-            cout << num << "    ";
-        }
-        cout << endl;
-
-
-        // Fill the vector of pairs with numbers from numbers and their ranks from epors
-        for (int i = 0; i < numbers.size(); ++i)
-        {
-            ranked_numbers.push_back(make_pair(numbers[i], i + 1)); // Use i+1 as the initial rank
-        }
-
-        // Sort ranked_numbers by numeric values according to the order in epors
-        sort(ranked_numbers.begin(), ranked_numbers.end(), [&](const pair<string, int>& a, const pair<string, int>& b) {
-            int posA = find(epors.begin(), epors.end(), a.first) - epors.begin();
-            int posB = find(epors.begin(), epors.end(), b.first) - epors.begin();
-            return posA < posB;
-            });
-
-        // Output the ordered numbers
-        cout << "Sorted set of alternatives to the first reference situation:" << endl;
-        for (const auto& num : ranked_numbers) {
-            cout << num.first << "\t";
-        }
-        cout << endl;
-
-        cout << "Single ordinal scale" << endl;
-        for (const auto& ep : epors) {
-            cout << ep << "\t";
-        }
-        cout << endl;
-
-        cout << endl;
-
-        cout << "Vector valuation (initial):" << endl;
-
-        for (int i = 0; i < 4; ++i)
-        {
-            for (int j = 0; j < 4; ++j)
-            {
-                cout << initial[i][j] << " ";
-            }
-            cout << endl;
-        }
-        cout << endl;
-
-        // Create a map to match the numbers from epors with their ordinal numbers
-        map<string, int> eporsToIndex;
-        for (int i = 0; i < epors.size(); ++i)
-        {
-            eporsToIndex[epors[i]] = i + 1;
-        }
-
-        // Create a new matrix initialByEpors
-        int initialByEpors[4][4];
-        for (int i = 0; i < 4; ++i)
-        {
-            for (int j = 0; j < 4; ++j)
-            {
-                int criterionIndex = initial[i][j] - 1;
-                string criterion = to_string(criteriaValues[criterionIndex][j]);
-                int rank = eporsToIndex[criterion];
-                initialByEpors[i][j] = rank;
-            }
-        }
-
-        // Output new matrix initialByEpors
-        cout << "Initial by a single ordinal scale:" << endl;
-        for (int i = 0; i < 4; ++i)
-        {
-            for (int j = 0; j < 4; ++j)
-            {
-                cout << initialByEpors[i][j] << " ";
-            }
-            cout << endl;
-        }
-
-        // Create a new matrix sortedInitialByEpors
-        int sortedInitialByEpors[4][4];
-        for (int i = 0; i < 4; ++i)
-        {
-            // Copy the line from initialByEpors to sortedInitialByEpors
-            for (int j = 0; j < 4; ++j)
-            {
-                sortedInitialByEpors[i][j] = initialByEpors[i][j];
-            }
-            // Sort the line
-            std::sort(sortedInitialByEpors[i], sortedInitialByEpors[i] + 4);
-        }
-        cout << endl;
-
-        // Output new matrix sortedInitialByEpors
-        cout << "Sorted Initial by a single ordinal scale:" << endl;
-        for (int i = 0; i < 4; ++i)
-        {
-            for (int j = 0; j < 4; ++j)
-            {
-                cout << sortedInitialByEpors[i][j] << " ";
-            }
-            cout << endl;
-        }
-        cout << endl;
-
-        // Find the line with the smallest numbers
-        int minRowIndex = 0;
-        for (int i = 1; i < 4; ++i)
-        {
-            if (sortedInitialByEpors[i][0] < sortedInitialByEpors[minRowIndex][0])
-            {
-                minRowIndex = i;
-            }
-        }
-        // Output the line with the smallest numbers
-        cout << "The best alternative:" << endl;
-        for (int j = 0; j < 4; ++j)
-        {
-            cout << sortedInitialByEpors[minRowIndex][j] << " ";
-        }
-        cout << endl;
-    }
-
-    else {
-        cout << "Invalid choice. Please enter 'R' or 'T'." << std::endl;
-    }
-
-    return 0;
-}
-
